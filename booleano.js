6a4b5286493dce7d3aa0a6d6276d0be65db4f285
leacau <leandrochena@gmail.com>
2022-03-19 12:21:13 -0300

    nada

---
+++ booleano.js
@@ -0,0 +1,17 @@
+let valor = true;
+
+if (valor) {
+    console.log("Esto funciona perfecto");
+}
+
+/* let unNumero = 5;
+
+// Con (unNumero == 5) comparamos si unNumero es igual a 5
+if (unNumero == 5) {
+    console.log("vas a ver este mensaje");
+}
+
+// Con (unNumero == 6) comparamos si unNumero es igual a 6
+if (unNumero == 6) {
+    console.log("no vas a ver este mensaje");
+} */
