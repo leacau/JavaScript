faf27d4aba9659042a775f03675bf1e8d27d8adf
leacau <leandrochena@gmail.com>
2022-03-22 22:20:57 -0300

    cambios

--- booleano.js
+++ booleano.js
@@ -1,8 +1,8 @@
-let valor = true;
+/* let valor = true;
 
 if (valor) {
     console.log("Esto funciona perfecto");
-}
+} */
 
 /* let unNumero = 5;
 
@@ -16,7 +16,7 @@ if (unNumero == 6) {
     console.log("no vas a ver este mensaje");
 } */
 
-let unColor = "Azul";
+/* let unColor = "Azul";
 
 // Con (unColor == "Rojo") comparamos si unColor es igual "Rojo"
 if (unColor == "Rojo") {
@@ -32,4 +32,102 @@ if (edad < 18) {
     alert("Usted no puede ingresar");
 } else {
     alert("Puedes ingresar");
+} */
+
+/* let edad = 19;
+
+if (edad < 10) {
+    console.log("No puedes entrar a la fiesta");
+} else if (edad < 18) {
+    console.log("Puedes ingresar con un adulto responsable");
+} else {
+    console.log("Puedes entrar solo");
+} */
+
+/* alert(
+    "¿Cuántos puntos tiene mi equipo? Ingresá los resultados de los partidos y enterate"
+);
+
+let cantGanados = parseInt(prompt("Cantidad de partidos ganados"));
+let cantEmpatados = parseInt(prompt("Cantidad de partidos empatados"));
+const puntEmpate = 1;
+const puntGanado = 3;
+
+var totalPuntos = puntEmpate * cantEmpatados + puntGanado * cantGanados;
+alert(`Los puntos que tiene tu equipo son: ${totalPuntos}`);
+
+if (totalPuntos >= 20) {
+    alert(`Tu equipo está clasificado al mundial`);
+} else if (15 <= totalPuntos < 20) {
+    alert(`Tu equipo tiene chances`);
+} else {
+    alert(`Ya no vas a gritar goles de tu equipo en el mundial`);
+} */
+
+/* let nombreIngresado = prompt("Ingresar nombre");
+let apellidoIngresado = prompt("Ingresar apellido");
+
+if (nombreIngresado != "" && apellidoIngresado != "") {
+    alert("Nombre: " + nombreIngresado + "\nApellido: " + apellidoIngresado);
+} else {
+    alert("Error: Ingresar nombre y apellido");
+} */
+
+alert("Hola! Vamos a registrar tu ingreso a la empresa.");
+alert("A continuación indicanos tu apellido y nombre");
+let apellido = prompt("¿Cuál es tu apellido?");
+let nombre = prompt("¿Cuál es tu nombre?");
+alert(
+    "A contnuación elije alguno de estos colores: rojo, verde, azul, amarillo, violeta "
+);
+let hoy = new Date();
+var minutes = parseInt(hoy.getMinutes());
+if (minutes < 10) {
+    var minutesN = "0" + minutes;
+} else minutesN = minutes;
+let hora = parseInt(hoy.getHours()) + ":" + minutesN;
+var colEleg = prompt("Elije entre: rojo, verde, azul, amarillo, violeta");
+while (
+    colEleg != "rojo" &&
+    colEleg != "verde" &&
+    colEleg != "azul" &&
+    colEleg != "amarillo" &&
+    colEleg != "violeta"
+) {
+    var colEleg = prompt("Elije entre: rojo, verde, azul, amarillo, violeta");
 }
+if (colEleg == "rojo") {
+    var color = "red";
+} else if (colEleg == "verde") {
+    var color = "green";
+} else if (colEleg == "azul") {
+    var color = "blue";
+} else if (colEleg == "amarillo") {
+    var color = "yellow";
+} else if (colEleg == "violeta") {
+    var color = "violet";
+} else {
+    var color = prompt("Elije entre: rojo, verde, azul, amarillo, violeta");
+}
+
+if (nombre != "" && apellido != "") {
+    document.write(
+        `<div style="color:${color}">Hola ${nombre} tu horario de ingreso es: ${hora}<br>Te deseamos un bonito día.</div>`
+    );
+}
+
+/* if (hora == "" || minut == "") {
+    alert("Por favor ingrese los datos solicitados");
+} else if (horario > "8:00") {
+    alert("Usted ha llegado tarde");
+} else if (horario > "7:45") {
+    alert("Intente llegar un poco antes");
+} else {
+    alert("Que tenga un bonito día!");
+}
+ */
+/* for (let i = 0; i < 10; i++) {
+    console.log("Hola");
+} */
+
+/* document.write("Acá dejas lo que quieras que devuelva el documento"); */
